69908ebb995811fadef68abe3756dc1ce3282b70
Abhijit <abhijitkayal10@gmail.com>
2026-08-12 14:22:51 +0530

    Changes

--- utils.js
+++ utils.js
@@ -1,22 +1,22 @@
 const fs = require("fs");
 
-
+const FILE_NAME = "tasks.json";
 const writeJSON = (tasks) => {
-  fs.writeFileSync("tasks.json", JSON.stringify(tasks, null, 2));
+  fs.writeFileSync(FILE_NAME, JSON.stringify(tasks, null, 2));
 };
 
 
 
 const readJSON = () => {
   try {
-    const fileData = fs.readFileSync("tasks.json", "utf8");
+    const fileData = fs.readFileSync(FILE_NAME, "utf8");
 
     return JSON.parse(fileData);
   } catch (error) {
     console.log(
       "⚠️ Could not read tasks.json. Starting with an empty task list.",
     );
-      writeJSON([]);
+    writeJSON([]);
     return [];
   }
 };
